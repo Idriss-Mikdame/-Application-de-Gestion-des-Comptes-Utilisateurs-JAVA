cdd3015d769852a81a2bcbf86e4e91ac9a8745c4
Idriss-Mikdame <mikdameidriss420@gmail.com>
2024-12-25 17:31:14 +0100

    la creation fonction Ajouter

--- src/GetiondeFonction.java
+++ src/GetiondeFonction.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GetiondeFonction {
+    public static Scanner input = new Scanner(System.in);
+    public static ArrayList<Utilisateur> stock = new ArrayList<>();
+
+    public static void add(){
+        System.out.print("Taper le Nom : ");
+        String nom = input.next();
+        System.out.print("Taper le AGE : ");
+        int age = input.nextInt();
+        System.out.print("Taper le EMAIL : ");
+        String email = input.next();
+        System.out.print("Taper le PASSWORD : ");
+        String motdepass = input.next();
+        System.out.print("Taper rolename (Admin-Employé-Client) : ");
+        String rolname = input.next();
+        Role role = new Role(rolname);
+        stock.add(new Utilisateur(nom,age,email,motdepass,role));
+
+    }
+
 }
