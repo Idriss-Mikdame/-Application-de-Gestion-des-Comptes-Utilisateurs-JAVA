670447c79e0a20c2d0c8e9cd729836ec2c08a5d5
Idriss-Mikdame <mikdameidriss420@gmail.com>
2024-12-25 17:50:01 +0100

    la creation Menu pour ajouter et afficher et supprimer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int choix;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Ajouter un utilisateur");
+            System.out.println("2. Afficher les utilisateurs");
+            System.out.println("3. Supprimer un utilisateur");
+            System.out.println("0. Quitter");
+            System.out.print("Votre choix : ");
+            choix = input.nextInt();
+            switch (choix){
+                case 1:
+                    GetiondeFonction.add();
+                    break;
+                case 2:
+                    GetiondeFonction.aff();
+                    break;
+                case 3:
+                    GetiondeFonction.supp();
+                    break;
+                case 4:
+                    System.out.println("Byee !!!!");
+                    break;
+                default:
+                    System.out.println("choix invalide ");
+
+            }
+        }while (choix !=4 );
+
+
+
+
 
     }
 }
